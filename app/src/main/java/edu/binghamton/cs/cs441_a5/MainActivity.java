afe53cabc9d185fdd978c455e8d70d3498139459
ryanegan85 <ryanegan85@gmail.com>
2019-07-17 07:54:51 -0400

    Added the ability to swipe and remove items off the RecyclerView, also accounts for the running total. Also added borders to each item on the list.

--- app/src/main/java/edu/binghamton/cs/cs441_a5/MainActivity.java
+++ app/src/main/java/edu/binghamton/cs/cs441_a5/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.binghamton.cs.cs441_a5;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mAdapter = new MyAdapter(this, numbers);
         recyclerView.setAdapter(mAdapter);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
 
         editNumber = findViewById(R.id.numberEdit);
         runningTotalText = findViewById(R.id.runningTotal);
@@ -77,4 +80,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            int x = numbers.get(viewHolder.getAdapterPosition());
+            numbers.remove(viewHolder.getAdapterPosition());
+            mAdapter.notifyDataSetChanged();
+
+            runningTotal -= x;
+            runningTotalText.setText("Running Total: " + runningTotal);
+        }
+    };
 }
